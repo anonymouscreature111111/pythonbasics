eb4f7fb9bd89a33e359a5b5f69f20be5e9546b6c
anonymouscreature111111 <anonymouscreature111111@gmail.com>
2026-04-14 14:27:52 +0600

    conditional statements

---
+++ date_time.py
@@ -0,0 +1,8 @@
+import datetime
+current_time = datetime.datetime.now()
+
+print('Time now at greenwhich meridian is:', end = "")
+print(current_time)
+
+import calendar
+print("\n", calendar.calendar(2021))
