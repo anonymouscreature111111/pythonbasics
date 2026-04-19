d7f44daf4bc872660b8a72f5fff85ac1d2a1f2b0
anonymouscreature111111 <anonymouscreature111111@gmail.com>
2026-04-19 16:00:05 +0600

    Add files via upload

---
+++ numberswap1.py
@@ -0,0 +1,15 @@
+x = input("Enter value of x: ")
+y = input('Enter value of y: ')
+z = input("Enter value of z: ")
+
+temp = x 
+temp2 = z
+temp3 = y
+
+x = temp2
+y = temp
+z = temp3
+
+print('Value of x after swapping', x)
+print("Value of y after swapping", y)
+print('Value of z after swapping', z)
