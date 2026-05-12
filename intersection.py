d14708baecc0c92a5aad66b7a204882b58988f13
anonymouscreature111111 <anonymouscreature111111@gmail.com>
2026-05-12 14:28:07 +0600

    datastructures_2

---
+++ intersection.py
@@ -0,0 +1,8 @@
+setc1 = {"green","blue"}
+setc2 = {"yellow","blue"}
+print("Original sets")
+print(setc1)
+print(setc2)
+setc = setc1.intersection(setc2)
+print("\nIntersection of above sets")
+print(setc)
