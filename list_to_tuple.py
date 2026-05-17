550883ceb69c1693f6ccd44548ce5a702e853b41
anonymouscreature111111 <anonymouscreature111111@gmail.com>
2026-05-17 15:43:10 +0600

    Add files via upload

---
+++ list_to_tuple.py
@@ -0,0 +1,11 @@
+first = input("First name: ")
+last = input("Last name: ")
+age = input("Age: ")
+height = input("Height: ")
+weight = input("Weight: ")
+subject = input("Favourite subject: ")
+
+details = (first, last, age, height, weight, subject)
+details_list = list(details)
+
+print(details_list)
